a4687c2d5abddf04ea9bb66cd0a5b13f5fc223d8
Alexey Artush <artush.alexey@gmail.com>
2022-01-07 14:10:11 +0300

    create basket

--- src/components/cart-block/cart-block.js
+++ src/components/cart-block/cart-block.js
@@ -1,13 +1,22 @@
-import React from "react";
+import React, { useState } from "react";
+import { useSelector } from "react-redux";
+import {CartMenu} from '../cart-menu'
 import {BiCartAlt} from 'react-icons/bi'
+import { calcTotalPrice } from "../utils";
+import {ItemsInCart} from '../items-in-cart'
 import './cart-block.css'
 
 
 export const CartBlock = () => {
+  const [isCartMenuVisible, setIsCartMenuVisible] = useState(false)
+  const items = useSelector(state => state.cart.itemsInCart)
+  const totalPrice = calcTotalPrice(items)
   return (
     <div className="cart-block">
-     <BiCartAlt size={25} className="car-block__icon"/>
-     <span className="cart-block__total-price">2333 р</span>
+      <ItemsInCart quantity ={items.length}/>
+     <BiCartAlt size={25} className="car-block__icon" onClick={() => setIsCartMenuVisible(!isCartMenuVisible)}/>
+     {totalPrice > 0 ? <span className="cart-block__total-price">{totalPrice} р</span> : null}
+     {isCartMenuVisible == true ? <CartMenu items={items} onClick={()=> null}/> : null}
     </div>
   )
 }
