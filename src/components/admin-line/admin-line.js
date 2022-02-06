a928db47b3e33d8d97cb640cb6bd7cce619b525b
Alexey Artush <artush.alexey@gmail.com>
2022-02-06 23:37:42 +0300

    merge

--- src/components/admin-line/admin-line.js
+++ src/components/admin-line/admin-line.js
@@ -1,37 +1,45 @@
 import React from "react";
 import { useEffect, useState } from "react";
-import { ModalWindow } from "../modal/modal";
 import { Button } from "../../components/button";
-import "./admin-line.css"
+import "./admin-line.css";
 
-export const AdmineLine = ({id, title, image, genres, price, video, description, onClick }) => {
-  
+export const AdmineLine = ({
+  id,
+  title,
+  image,
+  genres,
+  price,
+  video,
+  description,
+  onClick,
+}) => {
   useEffect(() => {
-    getGames()
-  }, [])
+    getGames();
+  }, []);
 
-  const [games, setGames] = useState([])
+  const [games, setGames] = useState([]);
 
   const getGames = async () => {
-    const response = await fetch(`http://localhost:3001/GAMES`)
-    const data = await response.json()
-    setGames(data)
-  }
+    const response = await fetch(`http://localhost:3001/GAMES`);
+    const data = await response.json();
+    setGames(data);
+  };
 
-        return (
-        <div className="admine-line" key={id}>
-          <div className="admine-line__cart">
-            <div className="admine-line__graph">Игра: {title}</div>
-            <div className="admine-line__graph">Ссылка на картинку: {image}</div>
-            <div className="admine-line__graph">Жанры: {genres}</div>
-            <div className="admine-line__graph">Цена: {price}$</div>
-            <div className="admine-line__graph">Ссылка на видео: {video}</div>
-            <div className="admine-line__graph">Описание игры: {description}</div>
-          </div>
-          <div className="admine-line__cart-button">
-            <Button size="m" onClick={onClick} id={id}>Удалить</Button>
-          </div>
-          
-        </div>
-      )
-    }
+  return (
+    <div className="admine-line" key={id}>
+      <div className="admine-line__cart">
+        <div className="admine-line__graph">Игра: {title}</div>
+        <div className="admine-line__graph">Ссылка на картинку: {image}</div>
+        <div className="admine-line__graph">Жанры: {genres}</div>
+        <div className="admine-line__graph">Цена: {price}$</div>
+        <div className="admine-line__graph">Ссылка на видео: {video}</div>
+        <div className="admine-line__graph">Описание игры: {description}</div>
+      </div>
+      <div className="admine-line__cart-button">
+        <Button size="m" onClick={onClick} id={id}>
+          Удалить
+        </Button>
+      </div>
+    </div>
+  );
+};
